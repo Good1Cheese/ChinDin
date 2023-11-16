2d0f32156a506189f528aaa514f9cbddf116042d
Good1Cheese <2916675@gmail.com>
2023-11-16 20:53:53 +0300

    :sparkles: Added ugly design

--- htdocs/index.php
+++ htdocs/index.php
@@ -1,1 +1,24 @@
-<?php echo "hello world"; ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>ChinDin</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter&display=swap" rel="stylesheet">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css">
+</head>
+<body>
+    <main>
+        <h1 class="title">Lorem ipsum</h1>
+        <div class="search">
+            <input class="search-bar" type="search" placeholder="Search">
+        </div>
+    </main>
+</body>
+</html>
+
